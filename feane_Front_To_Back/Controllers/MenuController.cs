dfdaf938792476ceae4ed1ac47c74f627ec73ca0
javadzadefatima <fatimaej-mpa201@code.edu.az>
2025-12-23 12:59:39 +0400

    first commit

---
+++ feane_Front_To_Back/Controllers/MenuController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace feane_Front_To_Back.Controllers
+{
+    public class MenuController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ feane_Front_To_Back/Program.cs
@@ -0,0 +1,17 @@
+namespace feane_Front_To_Back
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddControllersWithViews();
+            var app = builder.Build();
+            app.UseStaticFiles();
+            app.MapDefaultControllerRoute();
+
+
+            app.Run();
+        }
+    }
+}
